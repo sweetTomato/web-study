c4420b2637a8c3c69ec797a179d4c8980ba90816
sweetTomato <lsylet@gmail.com>
2023-05-18 03:56:50 +0200

    movie review site

---
+++ node-study/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// const helmet = require('helmet');
+const app = express();
+const ejs = require("ejs");
+const db = require("./model/db");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/public", express.static(__dirname + "/public"));
+
+// app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded());
+
+const mainRouter = require('./router/mainRouter');
+app.use('/', mainRouter);
+
+app.listen(3000,function(req,res){
+    
+    db.sequelize.sync({force:false});
+    console.log("서버가 실행되고 있습니다.");
+})
